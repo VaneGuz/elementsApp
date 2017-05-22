b42a584ad4a24de5da95e25636f1d3eb1c81c26f
Vanesa Guzmán <vanesa.guzman@udea.edu.co>
2017-05-22 15:22:39 -0500

    Primer commit, cardview ready

---
+++ app/src/main/java/vane/micasa/co/elementsapp/FirebaseReferences.java
@@ -0,0 +1,11 @@
+package vane.micasa.co.elementsapp;
+
+/**
+ * Created by Michael Garcia on 10/05/2017.
+ */
+
+public class FirebaseReferences {
+    final public static String PERFUME_REFERENCE = "Perfume";
+    final public static String PEDIDO_REFERENCE = "Pedido ";
+
+}
---
+++ app/src/main/java/vane/micasa/co/elementsapp/Pedido.java
@@ -0,0 +1,8 @@
+package vane.micasa.co.elementsapp;
+
+/**
+ * Created by Michael Garcia on 10/05/2017.
+ */
+
+public class Pedido {
+}
